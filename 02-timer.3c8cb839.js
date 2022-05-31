ecacf2ec091d3f7cc74b28304928d4a25bc3942b
OxanaDemova <93287016+OxanaDemova@users.noreply.github.com>
2022-05-31 20:47:59 +0000

    Deploying to gh-pages from @ OxanaDemova/goit-js-hw-09@a32dd956a032e7c750ddbea117f9589d36236d01 🚀

--- 02-timer.3c8cb839.js
+++ 02-timer.3c8cb839.js
@@ -17,6 +17,6 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./types/options":"lJVm","./l10n/default":"SFEe","./utils":"U4Vg","./utils/dom":"UwHZ","./utils/dates":"viQd","./utils/formatting":"KtbI","./utils/polyfills":"pFfd"}],"c0Wa":[function(require,module,exports) {
 
 },{}],"d2uN":[function(require,module,exports) {
-"use strict";var e=t(require("flatpickr"));function t(e){return e&&e.__esModule?e:{default:e}}require("flatpickr/dist/flatpickr.min.css");const r={startBtn:document.querySelector("button[data-start]"),dataPicker:document.querySelector("#datetime-picker")},a=Date.now(),n={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){if(e<=a)return alert("Please choose a date in the future");console.log(e[0])}};(0,e.default)(r.dataPicker,n);
+"use strict";var t=e(require("flatpickr"));function e(t){return t&&t.__esModule?t:{default:t}}require("flatpickr/dist/flatpickr.min.css");const a={startBtn:document.querySelector("button[data-start]"),dataPicker:document.querySelector("#datetime-picker")};function r(){a.startBtn.disabled=!0}function n(){a.startBtn.disabled=!1}r();const i=Date.now(),c={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(t){if(t[0]<=i)return r(),alert("Please choose a date in the future");n(),console.log(t[0])}};(0,t.default)(a.dataPicker,c);
 },{"flatpickr":"BKNP","flatpickr/dist/flatpickr.min.css":"c0Wa"}]},{},["d2uN"], null)
-//# sourceMappingURL=/goit-js-hw-09/02-timer.1a9d887d.js.map
+//# sourceMappingURL=/goit-js-hw-09/02-timer.3c8cb839.js.map
